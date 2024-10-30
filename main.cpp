f78ad7448966c9b9fefd85e5d8b9aaaf99e37fc1
Odin Lyngsgård <odinlyngsgard01@gmail.com>
2024-10-30 23:29:46 +0100

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,115 @@
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+vector<vector<int>> createBoard(int size) {
+    vector<vector<int>> board;
+    for (int i = 0; i < size; i++) {
+        vector<int> row;
+        for (int j = 0; j < size; j++) {
+            row.push_back(0);
+        }
+        board.push_back(row);
+    }
+    return board;
+}
+
+void printBoard(vector<vector<int>> board) {
+    string divider = "+";
+    for (int i = 0; i < board.size(); i++) {
+        divider = divider + "-+";
+    }
+    cout << divider << endl;
+    for (int i = 0; i < board.size(); i++) {
+        string row = "|";
+        for (int j = 0; j < board[i].size(); j++) {
+            string x = "";
+            if (board[i][j] == 0) {
+                x = " |";
+            } else if (board[i][j] == 1) {
+                x = "s|";
+            } else {
+                x = "*|";
+            }
+            row = row + x;
+        }
+        cout << row << endl;
+        cout << divider << endl;
+    }
+}
+
+void placeApple(vector<vector<int>> &board) {
+    srand(time(0));
+
+    bool validPlace = false;
+
+    int randX = 0;
+    int randY = 0;
+
+    while (!validPlace) {
+        randX = rand() % board.size();
+        randY = rand() % board.size();
+
+        if (board[randX][randY] != 1) {
+            validPlace = true;
+        }
+    }
+    board[randX][randY] = 3;
+}
+
+void moveSnake(int oldPos[], int newPos[], vector<vector<int>> &board) {
+    board[oldPos[0]][oldPos[1]] = 0;
+    board[newPos[0]][newPos[1]] = 1;
+}
+
+void getMovement(int &direction) {
+    int* x = &direction;
+    int* y = &direction + 1;
+    char directionChar;
+    cin >> directionChar;
+    if (directionChar == 'w') {
+        *x = 0;
+        *y = 1;
+    } else if (directionChar == 'a') {
+        *x = -1;
+        *y = 0;
+    } else if (directionChar == 's') {
+        *x = 0;
+        *y = -1;
+    } else if (directionChar == 'd') {
+        *x = 1;
+        *y = 0;
+    }
+}
+
+
+int main()
+{
+    int direction[] = {1,0};
+    int size = 9;
+    vector<vector<int>> board = createBoard(size);
+
+    int snakeHead[] = {0,0};
+    placeApple(board);
+
+    board[snakeHead[0]][snakeHead[1]] = 1;
+
+    printBoard(board);
+
+    bool playing = true;
+    cout << "Do you want to play the snake?" << endl;
+
+    while (playing) {
+        cout << "" << endl;
+        printBoard(board);
+        int oldPos[] = {snakeHead[0],snakeHead[1]};
+        snakeHead[0] = snakeHead[0] + direction[0];
+        snakeHead[1] = snakeHead[1] + direction[1];
+        moveSnake(oldPos, snakeHead, board);
+        getMovement(direction[0]);
+    }
+    return 0;
+}
